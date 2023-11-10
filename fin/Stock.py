18fd6dd4cc02dcac42454f61a054eda8c6a501bd
DelinM <delinmuray@gmail.com>
2023-11-10 17:07:57 -0500

    Update Stock.py

--- fin/Stock.py
+++ fin/Stock.py
@@ -4,15 +4,21 @@ import yfinance
 
 class Stock:
 
-    def __init__(self, stock_name: str, quantity: int, trade_date: dt.datetime):
-        """
-        Assuming trade price is the closing price of the stock on the trade date
-        """
+    def __init__(self, stock_name: str):
+
         self.stock_name = stock_name
-        self.quantity = quantity
-        self.trade_date = trade_date
+        Stock._validate_stock(stock_name)
         self.trade_price = None
 
-    def get_trade_price(self) -> None:
-        yfinance.Ticker(self.stock_name).history(period="7d", interval="1d")
-        return stock_data
+    @staticmethod
+    def _validate_refresh_stock(stock_name: str):
+        stock_ticker = yfinance.Ticker(stock_name)
+        stock_info = stock_ticker.history(period="1d")
+        if len(stock_info) == 0:
+            raise Exception("Invalid stock, enter a valid stock")
+        else:
+
+
+
+
+random_stock = Stock("AAPL")
