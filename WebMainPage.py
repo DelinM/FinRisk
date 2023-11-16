854a569e75c451bfbe14f6f724db4f747ed6cb98
DelinM <delinmuray@gmail.com>
2023-11-15 19:36:33 -0500

    Update WebMainPage.py

--- WebMainPage.py
+++ WebMainPage.py
@@ -5,10 +5,9 @@ import stTools as tools
 from assets import Portfolio
 from assets import Stock
 from models.MonteCarloSimulator import Monte_Carlo_Simulator
-# import locale
-#
-# locale.setlocale(locale.LC_ALL, 'en_CA.UTF-8' )
+import locale
 
+locale.setlocale(locale.LC_ALL, '')
 
 st.set_page_config(
     page_title="FinRisk",
